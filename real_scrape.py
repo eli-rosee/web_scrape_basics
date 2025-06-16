7f87d4663290930b6f48ec9d960d371f60ba6dc7
eli-rosee <eli.j.rose.04@gmail.com>
2025-06-16 17:13:41 -0500

    finished web scraping the first webpage, learned the fundamentals of scraping

--- real_scrape.py
+++ real_scrape.py
@@ -1,14 +1,18 @@
+# imports
 import requests
 from bs4 import BeautifulSoup
 
+# use the python request tool to get the html version of the website and parse it with the lxml engine
 html_text = requests.get('https://www.scrapethissite.com/pages/simple/').text
 soup = BeautifulSoup(html_text, 'lxml')
 
+# find all instances of h3, in which the country names are all stored
 countries = soup.find_all('h3', class_='country-name')
 
 names = []
-for country in countries:
-    names.append(country.get_text().strip())
 
-for name in names:
-    print(name)
+# search through all of the h3 and grab the text, aka the country name, store it in a list, and print it
+for country in countries:
+    new_country = country.get_text().strip()
+    names.append(new_country)
+    print(new_country)
