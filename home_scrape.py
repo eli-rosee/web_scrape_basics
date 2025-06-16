a97f7a5dc8f1016370d76567a9f02416033386a0
eli-rosee <eli.j.rose.04@gmail.com>
2025-06-16 17:07:27 -0500

    finished web scraping the first webpage

--- home_scrape.py
+++ home_scrape.py
@@ -1,12 +1,36 @@
 from bs4 import BeautifulSoup
 
-with open('home.html', 'r') as html_file:
-    content = html_file.read()
+html_doc = """<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="title"><b>The Dormouse's story</b></p>
 
-    soup = BeautifulSoup(content, 'lxml')
-    course_cards = soup.find_all('div', class_='card')
-    for course in course_cards:
-        course_name = course.h5.text
-        course_price = course.a.text.split()[-1]
+<p class="story">Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
 
-        print(f'{course_name} costs {course_price}')
+<p class="story">...</p>
+"""
+
+soup = BeautifulSoup(html_doc, 'lxml')
+
+# print("Title full tage: " + str(soup.title))
+# print("soup.title.name: " + str(soup.title.name))
+# print("soup.title.string: " + str(soup.title.string))
+# print("soup.p: " + str(soup.p))
+# print("soup.p['class']: " + str(soup.p['class']))
+# print("soup.a: " + str(soup.a))
+# print("soup.find_all('a'): " + str(soup.find_all('a')))
+# print('soup.find(id="link3"): ' + str(soup.find(id="link3")))
+
+# for link in soup.find_all('a'):
+#     print(link.get('href'))
+
+print(soup.get_text())
+
+p = soup.p.string
+print(p)
+print(type(p))
+print(str(p))
+print(unicode(p))
--- real_scrape.py
+++ real_scrape.py
@@ -1,1 +1,14 @@
 import requests
+from bs4 import BeautifulSoup
+
+html_text = requests.get('https://www.scrapethissite.com/pages/simple/').text
+soup = BeautifulSoup(html_text, 'lxml')
+
+countries = soup.find_all('h3', class_='country-name')
+
+names = []
+for country in countries:
+    names.append(country.get_text().strip())
+
+for name in names:
+    print(name)
